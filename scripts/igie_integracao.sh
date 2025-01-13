bc8340f420d7fc581707865abd6b011be384569f
Filipi Vianna <filipi@pucrs.br>
2025-01-13 10:44:59 -0300

    Integracao com o IGIE.

---
+++ scripts/igie_integracao.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'Qw121314!' | su - indusmart -c 'export DISPLAY=:0.0; cd /home/indusmart/igie_builds_and_3rdParties/igie-install/bin/; ./igie.sh ./repaired/FACE_repaired_repaired.step'
+
+#echo 'Qw121314!' | su indusmart -c 'whoami'
+#echo 'Qw121314!' | ../../../indusmart/igie_builds_and_3rdParties/igie-install/bin/igie.sh
+#../indusmart/igie_builds_and_3rdParties/igie-install/bin/igie.sh ../indusmart/igie_builds_and_3rdParties/igie-install/bin/repaired/box_repaired_repaired.step
+#whoami
+#../indusmart/igie_builds_and_3rdParties/igie-install/bin/igie.sh ../indusmart/igie_builds_and_3rdParties/igie-install/bin/repaired/box_repaired_repaired.step
+#echo 'Qw121314!' | su indusmart -c '../../../indusmart/igie_builds_and_3rdParties/igie-install/bin/igie.sh ../indusmart/igie_builds_and_3rdParties/igie-install/bin/repaired/box_repaired_repaired.step'
+#echo 'Qw121314!' | su - indusmart -c '../indusmart/igie_builds_and_3rdParties/igie-install/bin/igie.sh ../indusmart/igie_builds_and_3rdParties/igie-install/bin/repaired/box_repaired_repaired.step'
+#echo 'Qw121314!' | su - indusmart -c '../../../indusmart/igie_builds_and_3rdParties/igie-install/bin/igie.sh ../home/indusmart/igie_builds_and_3rdParties/igie-install/bin/repaired/box_repaired_repaired.step'
